94c518123d2c973acf6905e59bd55bedff940b83
Ebubechukwu <dimobiblaise@gmail.com>
2020-07-27 06:02:53 +0300

    First Version Ready

--- ARDice/ViewController.swift
+++ ARDice/ViewController.swift
@@ -11,7 +11,8 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
+    var diceArray = [SCNNode]()
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
@@ -19,15 +20,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the view's delegate
         sceneView.delegate = self
+        //
+        //        //add light
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        sceneView.autoenablesDefaultLighting = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,7 +32,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        
+        configuration.planeDetection = .horizontal
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -46,30 +45,120 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-
+    
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+    //MARK: - to detect touches and handle touches
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first{
+            let touchLocation = touch.location(in: sceneView)
+            
+            let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+            
+            if let hitResult = results.first{
+               addDice(atLocation: hitResult)
+                
+                
+              
+                
+            }
+        }
+        
         
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+    //MARK: - add dice to plane
+    func addDice( atLocation location: ARHitTestResult){
+        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+                      
+                      if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true){
+                          
+                          diceNode.position = SCNVector3(
+                              location.worldTransform.columns.3.x,
+                              location.worldTransform.columns.3.y + diceNode.boundingSphere.radius,
+                              location.worldTransform.columns.3.z
+                          )
+                          
+                          diceArray.append(diceNode)
+                          
+                          sceneView.scene.rootNode.addChildNode(diceNode)
+                          
+                          //to rotatae dice
+                         roll(dice: diceNode)
+                      }
+        
         
     }
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
+    func rollALL() {
+        if !diceArray.isEmpty{
+            
+            for dice in diceArray {
+                roll(dice: dice)
+            }
+            
+        }
         
     }
+    
+    func roll(dice: SCNNode){
+        //to rotatae dice
+        let randomX = Float(Int.random(in: 1...4)) * (Float.pi/2)
+        let randomZ = Float(Int.random(in: 1...4)) * (Float.pi/2)
+        
+        dice.runAction(
+            SCNAction.rotateBy(
+                x: CGFloat(randomX * 5),
+                y: 0,
+                z: CGFloat(randomZ * 5),
+                duration: 0.5
+        )
+    )
+        
+    }
+    @IBAction func removeAllDice(_ sender: UIBarButtonItem) {
+        
+        if !diceArray.isEmpty{
+            for dice in diceArray{
+                dice.removeFromParentNode()
+            }
+        }
+    }
+    
+    @IBAction func rollAgain(_ sender: UIBarButtonItem) {
+        
+        rollALL()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        rollALL()
+    }
+    
+    
+    //MARK: - add a plane ARSCNViewDelegate Method
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        if anchor is ARPlaneAnchor{
+            print("plane detected")
+            
+            let planeAnchor = anchor as! ARPlaneAnchor
+            
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+            
+            let planeNode = SCNNode()
+            
+            planeNode.position = SCNVector3(planeAnchor.center.x, 0, planeAnchor.center.z)
+            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+            
+            let gridMaterial = SCNMaterial()
+            gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+            plane.materials = [gridMaterial]
+            planeNode.geometry = plane
+            
+            node.addChildNode(planeNode)
+        } else {
+            return
+        }
+    }
+    
 }
---
+++ README.md
@@ -0,0 +1,18 @@
+# ARDice
+This is an Augmented reality app that detects a plane and allows you to place a dice anywhere on the horizontal plane
+
+
+# Technologies used
+- Xcode
+- Swift
+- UIKit
+- ARKti
+- SceneKit
+
+
+# Screenshots
+
+![](Documentation/iphone11_1.png)
+
+
+
